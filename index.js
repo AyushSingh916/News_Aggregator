35a70a98661a1775c7c3f5416f33a4cb97a68006
Ayush <ayushsingh916924@gmail.com>
2023-09-08 22:54:33 +0530

    Dynamic features added

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const apiKey = '14071ce97be7431580610ca6fca27bbd';
 const API_URL = `https://newsapi.org/v2/top-headlines?country=in&apiKey=${apiKey}`;
+const SEARCH_API = 'https://newsapi.org/v2/everything?';
 
 const cards = document.getElementById('card-container');
+const search = document.getElementById('search');
+const form = document.getElementById("form");
+const submitBtn = document.getElementById('btn');
 
 getTopHeadLines(API_URL);
 
@@ -23,17 +27,47 @@ function showTopHeadLines(data) {
             card.classList.add('card');
 
             card.innerHTML = `
+            <div id="thumbnail">
                 <img src="${article.urlToImage}" alt="">
+                <p id = "author_name">${article.author}</p>
+            </div>
                 <div class="card-text">
-                    <h2>${article.title}</h2>
+                    <h3>${article.title}</h3>
                     <p>${article.description}</p>
                     <a href="${article.url}">Read More</a>
                 </div>
             `
-            // console.log(article);
+            console.log(article);
             cards.appendChild(card);
         })
     } else {
         throw new Error("Failed to get the articles");
     }
 }
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const searchTerm = search.value;
+
+    if (searchTerm && searchTerm !== "") {
+        getTopHeadLines(SEARCH_API + `q=${searchTerm}&apiKey=14071ce97be7431580610ca6fca27bbd`);
+
+        search.value = "";
+    } else {
+        window.location.reload();
+    }
+})
+
+submitBtn.addEventListener('click', () => {
+
+    const searchTerm = search.value;
+
+    if (searchTerm && searchTerm !== "") {
+        getTopHeadLines(SEARCH_API + `q=${searchTerm}&apiKey=14071ce97be7431580610ca6fca27bbd`);
+
+        search.value = "";
+    } else {
+        window.location.reload();
+    }
+})
