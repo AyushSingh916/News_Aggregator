53c305fd7a31e9dc4f0e84ea5e2071bb22165616
Ayush <ayushsingh916924@gmail.com>
2023-09-10 00:24:22 +0530

    Adding cards in form of grid

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const cards = document.getElementById('card-container');
 const search = document.getElementById('search');
 const form = document.getElementById("form");
 const submitBtn = document.getElementById('btn');
+const Home = document.getElementById("Home");
+const Contact = document.getElementById("Contact");
 
 getTopHeadLines(API_URL);
 
@@ -25,36 +27,35 @@ function showTopHeadLines(data) {
         articles.forEach((article, index) => {
             const card = document.createElement('div');
             card.classList.add('card');
-            card.id = `card-${index}`; // Add a unique identifier to each card
+            const imageAddress = (article.urlToImage != null) ? article.urlToImage : 'https://images.unsplash.com/photo-1586339949916-3e9457bef6d3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80';
+            
+            // Trim the description to a certain length
+            const maxDescriptionLength = 150; // Adjust the desired length
+            const description = article.description && article.description.length > maxDescriptionLength
+                ? article.description.slice(0, maxDescriptionLength) + '...' // Add ellipsis
+                : article.description;
+            // description = (description === "null") ? "lorem50" : description;
 
             card.innerHTML = `
-            <div id="thumbnail">
-                <img src="${article.urlToImage}" alt="">
-                <p id="author_name">${article.author}</p>
-                <a href="${article.url} id = "visSrc">Visit Source</a>
-            </div>
-            <div class="card-text">
-                <h3>${article.title}</h3>
-                <p>${article.description}</p>
-                <hr>
-                <button class="readMore" id = "read">Read More</button>
-                <div class="hidden-content" id="content-${index}">${article.content}</div> <!-- Hidden content -->
-            </div>
+            <h3>${article.title}</h3>
+            <div id = "thumbnail_desc">
+                <div id="thumbnail">
+                    <img src="${imageAddress}" alt="">
+                    <p id="author_name">${article.author}</p>
+                </div>
+                <div id="card-text">
+                    <p>${description}</p> 
+                    <a href="${article.url}">Read More</a>
+                </div>
+            <\div>
             `;
 
             cards.appendChild(card);
         });
-
-        // Add event listeners for "Read More" buttons
-        const readMoreButtons = document.querySelectorAll('.readMore');
-        readMoreButtons.forEach((button, index) => {
-            button.addEventListener('click', () => {
-                toggleHiddenContent(index); // Toggle content when the button is clicked
-            });
-        });
     } else {
         throw new Error("Failed to get the articles");
     }
+    console.log(data);
 }
 
 form.addEventListener("submit", (e) => {
@@ -84,20 +85,11 @@ submitBtn.addEventListener('click', () => {
     }
 })
 
-function toggleHiddenContent(index) {
-    const content = document.getElementById(`content-${index}`);
-    const readMoreButton = document.querySelector(`#card-${index} .readMore`); // Select the button within the card
 
-    if (content) {
-        content.classList.toggle('show');
-        readMoreButton.classList.toggle('show');
+Home.addEventListener('click', () => {
+    getTopHeadLines(API_URL);
+})
 
-        if (content.classList.contains('show')) {
-            // If content is shown, set button text to "Read Less"
-            readMoreButton.innerText = 'Read Less';
-        } else {
-            // If content is hidden, set button text to "Read More"
-            readMoreButton.innerText = 'Read More';
-        }
-    }
-}
+Contact.addEventListener('click', () => {
+    window.location.href = 'mailto:your_email@example.com';
+})
